930839b8c491a9a205d0c4f719d652a82b611494
Andrew Musk <amusk@lendingclub.com>
2018-07-06 22:55:49 -0700

    Re-creating for heroku

---
+++ README.md
@@ -0,0 +1,2 @@
+Created a simple node web server using express. 
+Learnt about handlebars and using view templates and partials. 
---
+++ server.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+const port = process.env.PORT || 3000;
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine','hbs');
+
+
+
+app.use((req, res, next) => {
+	var now = new Date().toString();
+	var log = `${now}: ${req.method} ${req.url}`;
+	console.log(log);
+	fs.appendFile('server.log',log+ '\n', (err) => {
+		if (err)
+		{
+			console.log('Unable to append to server');
+		}
+	});
+	next();
+});
+
+// app.use((req,res,next)=> {
+// 	res.render('maintenance.hbs');
+// 	next();
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+
+
+hbs.registerHelper('getCurrentYear', () => {
+	return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+	return text.toUpperCase();
+});
+
+app.get('/',(req,res) => {
+	//res.send('<h1>Hello Express! This is Andrew Musk</h1>');
+	res.render('home.hbs', {
+		pageTitle: 'Welcome Home',
+		currentYear: new Date().getFullYear(),
+		welcomeMessage: "Welcome to my website"
+	});
+});
+
+app.get('/about',(req,res) => {
+	//res.send('<h1>Hello Express! This is Andrew Musk</h1>');
+	res.render('about.hbs', {
+		pageTitle: 'About Page',
+		currentYear: new Date().getFullYear()
+	});
+});
+
+app.get('/bad',(req,res) => {
+	//res.send('<h1>Hello Express! This is Andrew Musk</h1>');
+	res.send({
+		error: "THis is not a good thing"
+	});
+});
+
+
+app.listen(port, () => {
+	console.log(`Server is up and running on ${port}`);
+});
